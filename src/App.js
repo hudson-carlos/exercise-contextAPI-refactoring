a03a10559d8cb49a3ca848204309703bad7060f0
Hudson Carlos <hc88806@gmail.com>
2021-04-20 13:58:18 -0400

    exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import Mycontext from './comoponets/myContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      ...this.state,
+      [car]: side,
+    });
+  }
+
+  render (){
+    const valueContext = {
+      ...this.state,
+      moveCar: this.moveCar,
+    };
+    return (
+      <Mycontext.Provider value={valueContext}>
+        <Cars />
+      </Mycontext.Provider>
+    );
+  }
 }
 
 export default App;
